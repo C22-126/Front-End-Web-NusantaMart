23001efc1ab8d97be9edfd9d830e69f573670016
zamzamnurahman <kazamnn1912@gmail.com>
2022-12-03 22:57:07 +0700

    add page login

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, session
+import pymysql
+import secrets
 
 app = Flask(__name__)
+secret = secrets.token_urlsafe(32)
+app.secret_key = secret
+db = pymysql.connect(user="root", database='nusantamart')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+def hello_world():
+    return render_template('login.html')
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    nama = request.form['nama']
+    email = request.form['email']
+    password = request.form['password']
+    cursor = db.cursor()
+    query = "INSERT INTO `users`(`id`,`nama`,`email`,`password`) VALUES(null,%s,%s,%s)"
+
+    cursor.execute(query, (nama, email, password))
+    db.commit()
+
+    query = "SELECT `id` FROM `users` WHERE email = %s AND password = %s"
+    cursor.execute(query, (email, password))
+    data = cursor.fetchall()
+    session['user_id'] = data[0]
+
+    return redirect('/homepage')
+
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html', id=session['user_id'])
+
+
+@app.route('/profile')
+def profile():
+    query = "SELECT * FROM `users` WHERE id=%s"
+    cursor = db.cursor()
+    cursor.execute(query, (session['user_id']))
+    data = cursor.fetchone()
+    return render_template('profile.html', data=data)
 
 
 if __name__ == '__main__':
